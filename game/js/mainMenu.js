0feac393d338b653037e713c20788a8751a5433c
Noelle Correa <Noelle Correa>
2017-05-31 15:25:49 -0700

    Modified main menu, added optional cutscene when game starts

--- game/js/mainMenu.js
+++ game/js/mainMenu.js
@@ -7,19 +7,14 @@ MainMenu.prototype =
         //Background
         game.background = game.add.image(0,0,'MenuBG');
 
-        //Centered Play Button
-        /*playButton = game.add.button(400, 350, 'play', this.playTime, this);
-        playButton.anchor.x = 0.5;
-        playButton.anchor.y = 0.5;*/
-        var startText = new AddOption(game, 350, 350, 'Play', 'Level1Part1');
+        // add menu options
+        var startText = new AddOption(game, 350, 350, 'Play', 'Cutscene');
         game.add.existing(startText);
 
         var instrText = new AddOption(game, 290, 430, 'Instructions', 'Instructions');
         game.add.existing(instrText);
+
+        //var sceneText = new AddOption(game, 290, 510, 'Cutscene', 'Cutscene');
+        //game.add.existing(sceneText);
     },
-    /*playTime: function()
-    {
-        //Starts game on button press
-        game.state.start('Level1Part1');
-    }*/
 };
