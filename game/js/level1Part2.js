e74089b1941910fdb9b88b22e639e9c1d0698921
Daniel Martin <dagmarti@ucsc.edu>
2017-06-05 16:28:04 -0700

    fixed font and font colors

--- game/js/level1Part2.js
+++ game/js/level1Part2.js
@@ -330,7 +330,7 @@ Level1Part2.prototype =
       render: function() {
       //Prints out the timer
       if (levelTimer.running) {
-              game.debug.text("Time left: "+this.formatLevelTime(Math.round((levelTimerEvent.delay - levelTimer.ms) / 1000)), 32, 32, "#ffffff");
+              game.debug.text("Time left: "+this.formatLevelTime(Math.round((levelTimerEvent.delay - levelTimer.ms) / 1000)), 32, 32, "#000000");
           }
       //If the timer reaches 0, print this out
           else {
